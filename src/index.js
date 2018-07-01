c12cd117736a5b405a7cf0149b17e89f930b934d
MininMobile <notminin@gmail.com>
2018-07-01 23:32:57 +0200

    create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const dictionary = require("dictionary");
+
+new Promise((result, error) => {
+	if (process.argv[2] == "-e" || "--encrypt") {
+		result("encrypt argument");
+	} else if (process.argv[2] == "d" || "--decrypt") {
+		result("decrypt argument");
+	} else {
+		result("no arguments");
+	}
+}).then((res, err) => {
+	console.log(res);
+});
