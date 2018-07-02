15b2d552ec4b8b00d532c55f3141f26708b51725
MininMobile <notminin@gmail.com>
2018-07-02 12:43:29 +0200

    add tobinary function

--- src/dictionary.js
+++ src/dictionary.js
@@ -7,12 +7,34 @@ class Dictionary {
 			let letter = this.letters[i];
 
 			this.dictionary[letter] = ToBinary(i);
+			console.log(this.dictionary[letter]);
 		}
 	}
 }
 
 function ToBinary(number) {
-	
+	let result = "00000";
+
+	while (number > 0) {
+		if (number / 16 != 0) {
+			result[0] = 1;
+			number -= 16;
+		} else if (number / 8 != 0) {
+			result[1] = 1;
+			number -= 8;
+		} else if (number / 4 != 0) {
+			result[2] = 1;
+			number -= 4;
+		} else if (number / 2 != 0) {
+			result[3] = 1;
+			number -= 2;
+		} else if (number / 1 != 0) {
+			result[4] = 1;
+			number -= 1;
+		}
+	}
+
+	return result;
 }
 
 function ToNumber(binary) {
