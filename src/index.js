eeb5f20bb435f0630f33f6fe0b5d54d3294d3e49
MininMobile <notminin@gmail.com>
2018-07-02 14:04:01 +0200

    add help and updates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 new Promise((result, error) => {
 	if (process.argv[2] == "-e" || process.argv[2] == "--encrypt") {
+		console.log("Encoding please wait...");
 		fs.readFile(process.argv[3], "utf-8", (err, data) => {
 			let dict = new dictionary.Dictionary();
 			let result = "";
@@ -17,9 +18,10 @@ new Promise((result, error) => {
 				}
 			}
 
-			fs.writeFile(process.argv[4], result, (err) => { });
+			fs.writeFile(process.argv[4], result, (err) => { console.log("Operation complete!"); });
 		});
 	} else if (process.argv[2] == "-d" || process.argv[2] == "--decrypt") {
+		console.log("Decoding please wait...");
 		fs.readFile(process.argv[3], "utf-8", (err, data) => {
 			let dict = new dictionary.Dictionary();
 			let result = "";
@@ -32,10 +34,12 @@ new Promise((result, error) => {
 				result += dict.letters[dictionary.ToNumber(binary)];
 			}
 
-			fs.writeFile(process.argv[4], result, (err) => { });
+			fs.writeFile(process.argv[4], result, (err) => { console.log("Operation complete!"); });
 		});
 	} else {
-		// no args
+		console.log("None or invalid arguments, help:");
+		console.log("\t-e --encrypt input output | encrypt input and output to output");
+		console.log("\t-d --decrypt input output | decrypt input and output to output");
 	}
 
 	result("");
