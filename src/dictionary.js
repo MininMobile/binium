a260f9c3b5e23a297e0065b732a499c785f6cd54
MininMobile <notminin@gmail.com>
2018-07-02 12:38:22 +0200

    add structure

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,3 +1,26 @@
-module.exports = {
-	Dictionary: null
+class Dictionary {
+	constructor() {
+		this.dictionary = {};
+		this.letters = "abcdefjhijklmnopqrstuvwxyz";
+
+		for (let i = 0; i < this.letters.length; i++) {
+			let letter = this.letters[i];
+
+			this.dictionary[letter] = ToBinary(i);
+		}
+	}
+}
+
+function ToBinary(number) {
+	
+}
+
+function ToNumber(binary) {
+
+}
+
+module.exports = exports = {
+	Dictionary: Dictionary,
+	ToBinary: ToBinary,
+	ToNumber: ToNumber
 }
