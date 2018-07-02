3d601af102432e7d19cf0fbd545a2911eadb6a62
MininMobile <notminin@gmail.com>
2018-07-02 13:27:26 +0200

    add encryption

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const dictionary = require("./dictionary");
+const fs = require("fs");
 
 new Promise((result, error) => {
 	if (process.argv[2] == "-e" || "--encrypt") {
-		console.log(dictionary.ToNumber("00011s"));
+		fs.readFile(process.argv[3], "utf-8", (err, data) => {
+			let dict = new dictionary.Dictionary();
+			let result = "";
+
+			for (let i = 0; i < data.length; i++) {
+				let character = data[i];
+
+				if (character == " ") {
+					result += " ";
+				} else {
+					result += dict.dictionary[character];
+				}
+			}
+
+			fs.writeFile(process.argv[4], result, (err) => { });
+		});
 	} else if (process.argv[2] == "d" || "--decrypt") {
 		result("decrypt argument");
 	} else {
